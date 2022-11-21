77691b2c08141c2a3c6672ef7bbb6952f05f88ac
Liioan <dev.davidbijok@gmail.com>
2022-11-21 22:25:27 +0100

    Created <PasswordCopy/> component, added framer-motion library, added react-copy-to-clipboard library

--- src/components/MainCard/MainCard.jsx
+++ src/components/MainCard/MainCard.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
+
+//.components
+import PasswordCopy from './PasswordCopy/PasswordCopy';
+
+//. styles
 import styles from './MainCard.module.css';
 
 export default function MainCard() {
+  const [password, setPassword] = useState('');
+
   return (
     <div className={styles.card}>
-      lorem
-      {/*  */}
-      {/*  */}
+      <h1 className={styles.title}>password generator</h1>
+      <PasswordCopy generatedPassword={password} />
+      <button onClick={() => setPassword('loremipsum')}>update</button>
     </div>
   );
 }
---
+++ src/components/MainCard/PasswordCopy/PasswordCopy.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import { AnimatePresence, motion } from 'framer-motion';
+
+//.styles
+import styles from './PasswordCopy.module.css';
+
+export default function PasswordCopy({ generatedPassword = '' }) {
+  const [isCopied, setIsCopied] = useState(false);
+  useEffect(() => {
+    if (isCopied === true)
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 2000);
+  }, [isCopied]);
+
+  const handleClick = () => {
+    if (generatedPassword.length) {
+      setIsCopied(true);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.bar}>
+        <span>{generatedPassword}</span>
+        <CopyToClipboard text={generatedPassword} onCopy={handleClick}>
+          <button className={styles.copy}>
+            <span className='material-symbols-outlined'>content_copy</span>
+          </button>
+        </CopyToClipboard>
+      </div>
+      <AnimatePresence>
+        {isCopied && (
+          <motion.span
+            className={styles.copied}
+            initial={{ opacity: 0, translateX: '-100%' }}
+            animate={{ opacity: 1, translateX: 0 }}
+            exit={{ opacity: 0, translateX: '100%' }}
+            transition={{ duration: 0.2, ease: 'easeOut' }}
+          >
+            copied to clipboard
+          </motion.span>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+}
